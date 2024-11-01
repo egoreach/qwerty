a76a504c0fbbb3c0bb3288e3c6a9fda7500c7e63
Egor Soldatov <egorka.sold@gmail.com>
2024-11-01 21:48:24 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from io import StringIO
+from pypdf import PdfReader
+from transformers import AutoTokenizer
+from transformers import AutoModelForSeq2SeqLM
+from transformers import pipeline
+
+
+checkpoint = "google-t5/t5-base"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
+
+summarizer = pipeline("summarization", model=model, tokenizer=tokenizer, device="cuda")
+
+
+st.title("Сириус.ИИ: задача от Биокада")
+
+
+def extract_text(article):
+    text = ""
+
+    reader = PdfReader(article)
+    for page in reader.pages:
+        text += page.extract_text()
+
+    return "summarize: " + text
+
+
+article = st.file_uploader("Загрузите статью для суммаризации", type="pdf")
+if article:
+    text = extract_text(article)
+    st.write(summarizer(text))
