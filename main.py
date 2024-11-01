9b527bb9d79b6cd5ca92c12d8e5354d00b0e5f79
Egor Soldatov <egorka.sold@gmail.com>
2024-11-01 22:39:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,5 @@ def extract_text(article):
 article = st.file_uploader("Загрузите статью для суммаризации", type="pdf")
 if article:
     text = extract_text(article)
+    st.write("**Сокращенный вариант статьи:** \n")
     st.write(summarizer(text[:2000])[0]['summary_text'])
